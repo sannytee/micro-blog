3d70c2104c7c60cdcc6b9a6bdb29981a68abf440
taiwosanni <tywosanni@gmail.com>
2020-09-08 12:34:24 +0100

    - create application factory
    - register auth blueprint in application factory
    
    - import model files

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.auth import auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    return app
+
+
+from app.models import *
