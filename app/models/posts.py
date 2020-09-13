2b1de909fb98957fc42aa65d09e089c81ce8394f
taiwosanni <tywosanni@gmail.com>
2020-09-13 11:49:48 +0100

    Update Models
    - create post model
    
    - update user model to have a self-referential many-to-many relationship
    
    - include method to get post from followed users

---
+++ app/models/posts.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from app import db
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
