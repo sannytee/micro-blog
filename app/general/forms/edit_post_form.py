d18909fb9db1be62362ead068b2621876adea9ac
taiwosanni <tywosanni@gmail.com>
2020-09-14 21:03:53 +0100

    General blueprint user profile
    - implement routes to view and edit profile, follow and unfollow user
    
    - create template files for user profile

---
+++ app/general/forms/edit_post_form.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, Length, ValidationError
+from wtforms import StringField, SubmitField, TextAreaField
+from app.models.users import User
+
+
+class EditPostForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    bio = TextAreaField('Bio', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Save')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditPostForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
